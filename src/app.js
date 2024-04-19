6bfecc005b648d4e5175358d4a5ddec927d9ad66
54ntu <chaudharysantaram8@gmail.com>
2024-04-19 18:47:40 +0545

    utils file configured

---
+++ src/app.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const cors = require('cors')
+const cookieParser = require('cookie-parser')
+
+const app = express();
+
+app.use(cors()) //configuring cors 
+
+app.use(express.json({limit:'16kb'}))
+
+app.use(express.urlencoded({extended:true,limit:"16kb"})) // this urlencoded helps to fetch data from URL
+
+app.use(express.static("public")) // static method helps to handle images, files
+app.use(cookieParser()) // to handle cookies
+
+
+module.exports = {app}
--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 require('dotenv').config()
+const { app } = require('./app');
 const { connectDB } = require("./db/db");
 
-    
+
 
 connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log(`server is listening at port ${process.env.PORT}`)
+
+    })
+})
+.catch((error)=>{
+    console.log('database connection failed!!!!!',error)
+})
